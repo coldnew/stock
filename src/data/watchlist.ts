1568b7b62a3e9d619693b76dc3b2708263711e99
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-09-03 22:37:36 +0800

    add standalone market data updater

---
+++ src/data/watchlist.ts
@@ -0,0 +1,5 @@
+export const watchlist = [
+  'AAPL', 'AMD', 'ARCC', 'BTCI', 'CHPY', 'DGRO', 'DRAM', 'GOOG', 'GPIQ', 'GPIX',
+  'IQQ', 'IWMI', 'JEPI', 'JEPQ', 'MAGS', 'MSFT', 'NVDA', 'OVL', 'QDVO', 'QQQ',
+  'QQQH', 'QQQI', 'SGOV', 'SPCX', 'SPYI', 'TQQQ', 'TSLA',
+];
